79ac74a79fadc87f679bee6df3a9d7ecadc96592
freezeX <39030019+freezeX@users.noreply.github.com>
2018-09-19 12:24:00 -0500

    untested

--- src/Clients/Client.java
+++ src/Clients/Client.java
@@ -2,6 +2,7 @@ package Clients;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.util.*;
+import java.util.logging.Logger;
 
 import com.oracle.webservices.internal.api.databinding.Databinding;
 
@@ -12,6 +13,7 @@ import Utilities.FileSystemException.AppendLimitException;
 import Utilities.FileSystemException.ChunkExistException;
 import Utilities.RMIInterface.ChunkInterface;
 import Utilities.RMIInterface.MasterInterface;
+import javafx.scene.chart.PieChart.Data;
 import sun.net.www.http.ChunkedInputStream;
 
 public class Client {
@@ -75,22 +77,27 @@ public class Client {
 		}
 		 
 	}
-	public void write(String path, byte[] data, long offset) {
+	public void write(String path, byte[] data, long offset) throws MalformedURLException, RemoteException, NotBoundException {
 		long length = data.length;
 		long startChunkID = offset/ChunkSize;
-		long endChunkID = ((offset + length - 1)/ChunkSize) + 1;
-		for (long i = startChunkID; i< endChunkID; i++) {
+		long endChunkID = (offset + length - 1)/ChunkSize;
+		long offsetData = 0; // Offset to split the data
+		for (long i = startChunkID; i<= endChunkID; i++) {
 			chunkLocation t = getChunkLocation(path, i);
-			long startIndex = (i==startChunkID)? offset%ChunkSize : 0;
-			long endIndex = ChunkSize;
-			pushData(t.chunkAddress, data, new DataID(clientID));
+			long startIndex = (i==startChunkID)? offset%ChunkSize : 0; // Offset in chunk
+			long lengthOfPushed = (i==startChunkID)? ChunkSize - startIndex  : ChunkSize;
+			long nextOffset = offsetData + lengthOfPushed ;
+			DataID dataID = new DataID(clientID);
+			pushData(t.chunkAddress, Arrays.copyOfRange(data, (int)offsetData, (int)nextOffset), dataID);
 			String primaryAddr = findLeaseHolder(t.chunkHandle);
 			ChunkInterface primary = (ChunkInterface) Naming.lookup(primaryAddr);
-			primary.write()
-			}
+			primary.write(path, i, t, dataID , startIndex);
 			
-		}
+			
+			offsetData = nextOffset;
+			}
 	}
+	
 	public long getFileLength(String path) {
 		return master.getFileLength(path);
 	}
@@ -115,6 +122,25 @@ public class Client {
 		return n;
 	}
 		
+	public void read(String path, long offset, byte[] data) throws MalformedURLException, RemoteException, NotBoundException {
+		long length = getFileLength(path);
+		long limit = Math.min(length, offset + data.length);
+		long startChunkIdx = offset/ ChunkSize;
+		long endChunkIdx = (limit-1)/ ChunkSize;
+		long writeIdx = 0;
+		for (long i = startChunkIdx; i <=endChunkIdx; i++) {
+			long startByteIdx = (i == startChunkIdx)? offset % ChunkSize : 0 ;
+			long endByteIdx = (i == endChunkIdx)? (limit-1)% ChunkSize : ChunkSize-1;
+			chunkLocation Chunklocation = getChunkLocation(path, i);
+			String primaryAddr = findLeaseHolder(Chunklocation.chunkHandle);
+			ChunkInterface primary = (ChunkInterface) Naming.lookup(primaryAddr);
+			byte[] dataFeed = new byte[(int) (endByteIdx - startByteIdx +1)];
+			primary.read(path,i, dataFeed,startByteIdx, endByteIdx);
+			System.arraycopy(dataFeed, 0, data, (int) writeIdx, dataFeed.length);
+			writeIdx = writeIdx + dataFeed.length;
+		}
+		
+	}
 	public void addChunk(String path, long chunkID) throws ChunkExistException {
 		master.addChunk(path,chunkID);
 	}
--- src/Utilities/RMIInterface/ChunkInterface.java
+++ src/Utilities/RMIInterface/ChunkInterface.java
@@ -7,5 +7,7 @@ import Utilities.DataID;
 public interface ChunkInterface extends Remote{
 	public void append(String path, long chunkID, chunkLocation cL, DataID d) throws NoEnoughSpaceException;
 	
-	public void write(String path, long chunkID, chunkLocation cL, DataID dataID, long startIndex, long endIndex);
+	public void write(String path, long chunkID, chunkLocation cL, DataID dataID, long startIndex);
+	
+	public void read(String path, long chunkID,byte[] data, long startByteIdx, long endByteIdx);
 }
